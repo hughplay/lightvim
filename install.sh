c157e63ff7d2bf4368b94dd9698335a06b02661d
Mr.Blue <silverhugh.77@gmail.com>
2019-01-09 13:17:12 +0800

    :rocket: Update install script.

--- install.sh
+++ install.sh
@@ -1,23 +1,48 @@
 #!/usr/bin/env bash
-rm -f ~/.vimrc
+if [ ! -d ~/.lightvim ]; then
+    git clone https://github.com/hughplay/lightvim.git ~/.lightvim
+else
+    cd ~/.lightvim && git pull && cd -
+fi
 
-wget https://raw.githubusercontent.com/hughplay/lightvim/master/.vimrc -O ~/.vimrc
-
+if [ -f ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
 if [ ! -d ~/.vim ]; then
     mkdir ~/.vim
+else
+    if [ ! -d ~/.vim.bak ]; then
+        mkdir ~/.vim.bak
+    fi
+    if [ -d ~/.vim/indent ]; then
+        mv ~/.vim/indent ~/.vim.bak/indent
+    fi
+    if [ -d ~/.vim/spell ]; then
+        mv ~/.vim/spell ~/.vim.bak/spell
+    fi
 fi
+
+ln -nfs ~/.lightvim/.vimrc ~/.vimrc
+ln -nfs ~/.lightvim/.vim/indent ~/.vim/indent
+ln -nfs ~/.lightvim/.vim/spell ~/.vim/spell
+
+# Create links for nvim
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
+else
+    if [ -L ~/.config/nvim ]; then
+        rm -f ~/.config/nvim
+    elif [ -d ~/.config/nvim ]; then
+        mv ~/.config/nvim ~/.vim.bak/nvim
+    fi
 fi
-rm -rf ~/.config/nvim
+
 ln -nfs ~/.vim ~/.config/nvim
 ln -nfs ~/.vimrc ~/.config/nvim/init.vim
 
 vim \
-    "source ~/.vimrc" \
-    "+set nomore" \
-    "+PlugInstall!" \
+    "+PlugInstall --sync" \
     "+PlugClean" \
-    "+qall"
+    "+qa"
 
 echo "Install complete!"
